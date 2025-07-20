f3accd4fc3b118c49f5d8782eb020e65aa657cca
radad_md <rafaghdioqbfb@gmail.com>
2025-07-20 23:03:20 +0300

    Create Instagram.py

---
+++ Instagram.py
@@ -0,0 +1,307 @@
+import requests,random,os;from user_agent import generate_user_agent
+import json,re
+import webbrowser,datetime
+webbrowser.open('https://t.me/c8vla')
+
+import os,requests,sys,time,datetime
+now = datetime.datetime.today()
+an = datetime.datetime.now()
+an2 = datetime.datetime(2029,10,29,0,00)
+hours = (now.hour)
+if an > an2 or an == an2:
+ print("\033[1;31mThe Date : "+ str(an))
+ print("\033[1;31mThe Exp Date : "+ str(an2))
+ time.sleep(1)
+ print('\t\n\033[1;31m  تــم ايــقــافـ الاداهـ مـن      ')
+ print('\033[1;31m @ar_ad_9 - @ar_ad_9  الـــمـــطـــوࢪ  ')
+ exit(0)
+else:
+ print("\n                             اشتراكك صالح لمدة                            ")
+ print("\033[1;31mالوقت حاليا : "+ str(an))
+ print('\n')
+ print('𓏳'*60)
+ print("\033[1;31m\nوقت الانتهاء : "+ str(an2))
+ print('')
+ print('𓏳'*60)
+ print('\t\n\033[1;31m @ar_ad_9 - @ar_ad_9  الـــمـــطـــوࢪ   ')
+ kilwa = '''''''''''print('𓏳'*50)'''''''''''
+
+#-------------[Done decode - 𓏺 haker_radad </> ᴍɢ]-----------------
+a1 = '\x1b[1;31m'  # أحمر
+a2 = '\x1b[1;34m'  # أزرق
+a3 = '\x1b[1;32m'  # أخضر
+a4 = '\x1b[1;33m'  # أصفر
+a5 = '\x1b[38;5;208m'  # برتقالي
+a6 = '\x1b[38;5;5m'  #MPMPK
+a30 = '\x1b[38;5;255m'  #RAVEN
+#----------[Done decode - 𓏺 haker_radad </> ᴍɢ]-------------
+LOGO=a4+"""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⡴⢧⣀⠀⠀⣀⣠⠤⠤⠤⠤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⠏⢀⡴⠊⠁⠀⠀⠀⠀⠀⠀⠈⠙⠦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣰⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢶⣶⣒⣶⠦⣤⣀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣰⠃⠀⠀Done decode - 𓏺 haker_radad </>⠀  ⠀⠀⠈⣟⠲⡌⠙⢦⠈⢧⠀
+⠀⠀⠀⣠⢴⡾⢟⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡴⢃⡠⠋⣠⠋⠀
+⠐⠀⠞⣱⠋⢰⠁⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠤⢖⣋⡥⢖⣫⠔⠋⠀⠀⠀
+⠈⠠⡀⠹⢤⣈⣙⠚⠶⠤⠤⠤⠴⠶⣒⣒⣚⣩⠭⢵⣒⣻⠭⢖⠏⠁⢀⣀⠀⠀⠀⠀
+⠠⠀⠈⠓⠒⠦⠭⠭⠭⣭⠭⠭⠭⠭⠿⠓⠒⠛⠉⠉⠀⠀⣠⠏⠀⠀⠘⠞⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⢤⣀⠀⠀⠀⠀⠀⠀⣀⡤⠞⠁⠀⣰⣆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠘⠿⠀⠀⠀⠀⠀⠈⠉⠙⠒⠒⠛⠉⠁⠀⠀⠀⠉⢳⡞⠉⠀⠀⠀⠀⠀
+"""
+print(LOGO)
+print(a5+"—"*60)
+#-----------[TOKEN, ID]---------------
+from rich.prompt import Prompt
+from rich.panel import Panel
+from rich import print as PRENTO
+TOK =input(f"{a5}[{a4}?{a5}]{a6} TOKEN:{a3}")
+PRENTO(Panel("•"*56, style="green",title="GOOD"))
+ID =input(f"{a5}[{a4}?{a5}]{a6} ID:{a3}")
+
+webbrowser.open('https://www.instagram.com/v4wt?igsh=MWJlMXV1NDI3b3kxdw==')
+
+os.system("clear")
+#------------[TIK TOK]----------------
+def TIK():
+    R="qwertyuiopasdfghjklzxcvbnm_."
+    RND="".join(random.choices(R,k=5))
+    url=f"https://www.tiktok.com/@{RND}"
+    headers = {"user-agent": generate_user_agent()}
+    req = requests.get(f"{url}", headers=headers).text
+    if "heart" in req:
+        if req.count("heart") == 1:
+            print(f"{a5}[{a4}•{a5}]{a3} GOOD:{RND}")
+            TIK=f"""< TIK TOK USER > 🫧
+[•] < {RND} >
+_______
+BY : @bsx_h2"""
+            requests.get('https://api.telegram.org/bot' + str(TOK) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(TIK))
+        else:
+            print(f"{a5}[{a3}•{a5}]{a1} BAD:{RND}")
+    else:
+        exit(f"{a5}[{a3}•{a5}]{a4} bad net")
+#-----------[Done decode - 𓏺 haker_radad </>]--------------
+def SNAP():
+    R="qwertyuiopasdfghjklzxcvbnm_."
+    RND="".join(random.choices(R,k=5))
+    url=f"https://www.snapchat.com/add/{RND}"
+    req=requests.get(url).text
+    if len(req)< 9000:
+        print(f"{a5}[{a4}•{a5}]{a3} GOOD:{RND}")
+        SNAP=f"""< SNAP CHAT USER > 🫧
+[•] < {RND} >
+_______
+BY : @ar_ad_9 - @ar_ad_9 - @ar_ad_9 """
+        requests.get('https://api.telegram.org/bot' + str(TOK) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(SNAP))
+    else:
+        print(f"{a5}[{a3}•{a5}]{a1} BAD:{RND}")
+#-----------[Done decode - 𓏺 haker_radad </>]---------------
+def TUBE():
+    R="qwertyuiopasdfghjklzxcvbnm_"
+    RND="".join(random.choices(R,k=5))
+    url=f"https://m.youtube.com/@{RND}"
+    req=requests.get(url).text
+    if len(req) < 1000:
+        print(f"{a5}[{a4}•{a5}]{a3} GOOD:{RND}")
+        TUB=f"""< YOU TUBE USER > 🫧
+[•] < {RND} >
+_______
+BY : @ar_ad_9 - @ar_ad_9 - @ar_ad_9 """
+        requests.get('https://api.telegram.org/bot' + str(TOK) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(TUB))
+    else:
+        print(f"{a5}[{a3}•{a5}]{a1} BAD:{RND}")
+#-----------[Done decode - 𓏺 haker_radad </>]-------------
+def INSTA():
+    R="qwertyuiopasdfghjklzxcvbnm_."
+    RND="".join(random.choices(R,k=5))
+    url=f"https://www.instagram.com/{RND}"
+    req=requests.get(url).text
+    if len(req) < 260000:
+        print(f"{a5}[{a4}•{a5}]{a3} GOOD:{RND}")
+        INSTA=f"""< INSTAGRAM USER > 🫧
+[•] < {RND} >
+_______
+BY : @ar_ad_9 - @ar_ad_9 - @ar_ad_9 """
+        requests.get('https://api.telegram.org/bot' + str(TOK) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(INSTA))
+    else:
+        print(f"{a5}[{a3}•{a5}]{a1} BAD:{RND}")
+def DISCORD():
+        while True:
+                z=random.randint(3,5)
+                SIN="".join(random.choice('123456789_.0qwertyuiopasdfghjklzxcvbnm.')for i in range(z))
+                url = "https://discord.com/api/v9/auth/register"
+                payload = json.dumps({
+                  "fingerprint": "1251066764461215794.0hGmY_Ie2DF1XDaXm0bTdSEDLaM",
+                  "email": "223chjdbn@gmail.com",
+                  "username": SIN,
+                  "global_name": "SIN IS STRONG ",
+                  "password": "12341234rfd",
+                  "invite": None,
+                  "consent": True,
+                  "date_of_birth": "2017-05-01",
+                  "gift_code_sku_id": None,
+                  "promotional_email_opt_in": True
+                })
+
+                headers = {
+                  'User-Agent':str(generate_user_agent()),
+                  'Accept-Encoding': "gzip, deflate, br, zstd",
+                  'Content-Type': "application/json",
+                  'sec-ch-ua': "\"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+                  'x-fingerprint': "1251066764461215794.0hGmY_Ie2DF1XDaXm0bTdSEDLaM",
+                  'x-debug-options': "bugReporterEnabled",
+                  'sec-ch-ua-mobile': "?1",
+                  'x-discord-timezone': "Asia/Baghdad",
+                  'x-discord-locale': "en-US",
+                  'sec-ch-ua-platform': "\"Android\"",
+                  'origin': "https://discord.com",
+                  'sec-fetch-site': "same-origin",
+                  'sec-fetch-mode': "cors",
+                  'sec-fetch-dest': "empty",
+                  'referer': "https://discord.com/register",                  'accept-language': "ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7",
+                  'priority': "u=1, i",
+                }
+
+                response = requests.post(url, data=payload, headers=headers).json()
+                if re.search('USERNAME_ALREADY_TAKEN',str(response)):
+                        print(Panel(f'[yellow ]BAD  USER :- {SIN}'))
+                elif 'The resource is being rate limited' in str(response):
+                        print(' 💔تم حضرك عد مره اخره بعد خمس دقائق')
+                        exit()
+
+                else:
+                        print(Panel(f'[green]GOOD USER : {SIN}'))
+                        requests.get("https://api.telegram.org/bot"+str(TOK)+"/sendMessage?chat_id="+str(ID)+"&text="+str(f'''
+        DISCORD USER
+        ====================
+        Done decode - 𓏺 haker_radad </> : {SIN}
+        =====================
+        py:- @ar_ad_9 - @ar_ad_9  '''))
+def TELE ():
+        import random
+        import requests
+        import socket
+        import time,pyfiglet
+        Z = '\033[1;31m' #احمر
+        X = '\033[1;33m' #اصفر
+        F = '\033[2;32m' #اخضر
+        C = "\033[1;97m" #ابيض
+        B = '\033[2;36m'#سمائي
+        E = "\033[0;90m" #رمادي
+        print('\n')
+        na = 'QWERTYU_IOPLKJHGFDSAMNBVCXZ_'
+
+        def check_user(username):
+            try:
+                 url = requests.get(f'https://t.me/{username}').text
+                 if 'tgme_username_link' in url:
+                        print(f'\033[1;32m good user -->: {username}')
+                        tlg = f"""
+        Done decode - 𓏺 haker_radad </> @ar_ad_9 - @ar_ad_9 
+        ᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓ
+        ᴍɢ ⌯ : {user}
+        ᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓ
+        ↝Done decode - 𓏺 haker_radad </>  • @ar_ad_9 - @ar_ad_9 
+        """
+
+                        requests.get("https://api.telegram.org/bot"+str(TOK)+"/sendMessage?chat_id="+str(ID)+"&text="+str(tlg))
+                 else:
+                        print(f'\033[1;31m erorr user -->: {username}')
+            except (requests.exceptions.RequestException, socket.timeout):
+                    print(f'\033[1;33m فصل النت انتضر خمس ثواني ')
+                    time.sleep(5)
+                    check_user(username)
+
+        while True:
+            u = str(''.join(random.choice(na) for i in range(1)))
+            us = str(''.join(random.choice(na) for i in range(1)))
+            a = str(''.join(random.choice(na) for i in range(1)))
+            user = u+a+us+u+u
+            check_user(user)
+#-------------[Done decode - 𓏺 haker_radad </>]-----------------
+LOG="""⠀⠀⠀⢠⣾⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣰⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢰⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣄⣀⣀⣤⣤⣶⣾⣿⣿⣿⡷
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀
+⣿⣿⣿⣿⣿ haker_radad 𓏺 @ar_ad_9⣿⣿⣿⣿⣿⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀
+⣿⣿⣿⡇⠀⡾⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀
+⣿⣿⣿⣧⡀⠁⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⢹⠉⠙⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣀⠀⣀⣼⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠀⠤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⠿⠋⢃⠈⠢⡁⠒⠄⡀⠈⠁⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⠟⠁⠀⠀⠈⠉⠉⠁⠀⠀⠀⠀⠈⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"""
+print(LOG)
+print(f"{a3}_"*60)
+#-------------[Done decode - 𓏺 haker_radad </>]----------------
+print(f"""__________________________
+{a5}[{a3}1{a5}]{a30} - {a4}|{a3}TIK TOK{a4}|
+{a5}[{a3}2{a5}]{a30} - {a4}|{a3}SNAP CHAT{a4}|
+{a5}[{a3}3{a5}]{a30} - {a4}|{a3}YOU TUBE{a4}|
+{a5}[{a3}4{a5}]{a30} - {a4}|{a3}INSTAGRAM{a4}|
+{a5}[{a3}5{a5}]{a30} - {a4}|{a3}DISCORD{a4}|
+{a5}[{a3}6{a5}]{a30} - {a4}|{a3}TELEGRAM{a4}|
+—————————————————————————""")
+def clr():
+    os.system("clear")
+X =input(f"{a5}[{a4}?{a5}]{a6} CHOSE:{a3}")
+if X =="1":
+    clr()
+    from cfonts import render, say
+    output = render('TIKTOK',colors=['green','white'], align='center')
+    print(output)
+    print(f"{a4}_"*60)
+    while True:
+        TIK()
+elif X =="2":
+    clr()
+    from cfonts import render, say
+    output = render('SNAP',colors=['green','white'], align='center')
+    print(output)
+    print(f"{a4}_"*60)
+    while True:
+        SNAP()
+elif X =="3":
+    clr()
+    from cfonts import render, say
+    output = render('TUBE',colors=['green','white'], align='center')
+    print(f"{a4}_"*60)
+    print(output)
+    print(f"{a4}_"*60)
+    while True:
+        TUBE()
+elif X =="4":
+    clr()
+    from cfonts import render, say
+    output = render('INSTA',colors=['green','white'], align='center')
+    print(output)
+    print(f"{a4}_"*60)
+    while True:
+        INSTA()
+elif X =="5":
+    clr()
+    from cfonts import render, say
+    output = render('DISCORD',colors=['green','white'], align='center')
+    print(output)
+    print(f"{a4}_"*60)
+    while True:
+        DISCORD()
+elif X =="6":
+    clr()
+    from cfonts import render, say
+    output = render('TELE',colors=['green','white'], align='center')
+    print(output)
+    print(f"{a4}_"*60)
+    while True:
+        TELE()
+else:
+    clr()
+    exit(f"{a5}[{a1}✘{a5}]{a3} BAD CHOSE")
